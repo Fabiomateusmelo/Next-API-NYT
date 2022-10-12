2bc9550eeec8a4ebcab345659d6aea95f73014dd
Fábio Mateus Melo <fabioceto@gmail.com>
2022-10-12 00:48:41 -0300

    0001

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [query, getQuery] = useState()
+  const router = useRouter()
+  const handleOnChange = e => getQuery(e.target.value)
+  const handleOnSubmit = e => {
+    e.preventDefault()
+    router.push(`/news/${query}`)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +24,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>News Feed</h1>
+
+        <form onSubmit={handleOnSubmit}>
+          <input type="text" onChange={handleOnChange} />
+        </form>
 
         <div className={styles.grid}>
           <Link href="/news/">
