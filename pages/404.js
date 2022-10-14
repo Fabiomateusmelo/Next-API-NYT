93cf8af71683890fd7019a6240db7ad622396710
Fábio Mateus Melo <fabioceto@gmail.com>
2022-10-14 10:58:55 -0300

    00001

--- pages/404.js
+++ pages/404.js
@@ -9,31 +9,26 @@ export default function NotFound() {
     router.back()
   }
   return (
-    <div className="container">
-      <Layout>
-        <br />
-        <a href="#" onClick={goBack}>
-          Back
-        </a>
-        <div className="container">
-          <h1>404 PAGE NOT FOUND</h1>
-          <Image src={gif} alt="not found" />
-          <style jsx>
-            {`
-              .container {
-                height: 80vh;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-direction: column;
-              },
-              .black {
-                background: black;
-              }
-            `}
-          </style>
-        </div>
-      </Layout>
-    </div>
+    <Layout>
+      <br />
+      <a href="#" onClick={goBack}>
+        Back
+      </a>
+      <div className="container">
+        <h1>404 PAGE NOT FOUND</h1>
+        <Image src={gif} alt="not found" />
+        <style>
+          {`
+            .container {
+              height: 80vh;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-direction: column;
+            }
+          `}
+        </style>
+      </div>
+    </Layout>
   )
 }
