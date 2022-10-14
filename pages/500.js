94b6e7c4202f811a045ff70835a90d16f0db467a
Fábio Mateus Melo <fabioceto@gmail.com>
2022-10-14 17:11:07 -0300

    0009

---
+++ pages/500.js
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import gif from '../public/not-found.gif'
+import Layout from '../components/Layout'
+
+export default function NotFound500() {
+  const router = useRouter()
+  const goBack = () => {
+    router.back()
+  }
+  return (
+    <Layout>
+      <div className="container">
+        <h1>404 PAGE NOT FOUND</h1>
+        <Image src={gif} alt="not found" />
+        <a href="#" onClick={goBack}>
+        Back
+        </a>
+        <style>
+          {`
+            .container {
+              height: 80vh;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-direction: column;
+            }
+          `}
+        </style>
+      </div>
+    </Layout>
+  )
+}
