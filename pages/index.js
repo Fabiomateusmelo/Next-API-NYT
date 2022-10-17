43f79d73ce2d469fe0b614b6409d9ab4837f5185
Fábio Mateus Melo <fabioceto@gmail.com>
2022-10-17 15:04:08 -0300

    0001

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
