451f6c7fced70b1caafeb7e22f79be77aa3fb9b4
Fábio Mateus Melo <fabioceto@gmail.com>
2022-10-17 15:16:16 -0300

    0004

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
@@ -68,12 +69,15 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a 
-          href="https://fabiomateusmelo.github.io/Portfolio-Fabio/"
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <span className={styles.logo}>{'Fábio Melo'}</span>
+          Powered by{' Fábio Melo'}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
         </a>
       </footer>
     </div>
